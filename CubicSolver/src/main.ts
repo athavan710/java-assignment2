ed7a70fcbe92cdf5eebfa1200931122c0259e115
Athavan <athavan.mahadevan@student.tdsb.on.ca>
2026-03-07 12:31:16 -0500

    Added variables

--- CubicSolver/src/main.ts
+++ CubicSolver/src/main.ts
@@ -1,24 +1,20 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const form = document.getElementById("cubicsolver") as HTMLFormElement;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+form?.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  const formData = new FormData(form);
+
+  const a: number = Number(formData.get("a"));
+  const b: number = Number(formData.get("b"));
+  const c: number = Number(formData.get("c"));
+  const d: number = Number(formData.get("d"));
+
+  const p = ((3 * a * c) - (b * b)) / (3 * a * a);
+  const q = ((27 * (a * a) * d) - (9 * a * b * c) + (2 * b * b * b)) / (27 * a * a * a)
+  const discriminant = (q / 2) **2 + (p / 2) **3;
+
+  
+
+
+})
